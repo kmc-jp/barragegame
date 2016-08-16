c9d03c89365c7e98bed00851ce08ecca5b23ea21
toruyoh <tlrtoruyo@fch.ne.jp>
2016-08-16 19:34:48 +0900

    練習用プログラム
    toruyohの練習用のプログラムです

---
+++ toruyohpractice/Game1/Game1.cs
@@ -0,0 +1,172 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+
+namespace Game1
+{
+    /// <summary>
+    /// This is the main type for your game.
+    /// </summary>
+    public class Game1 : Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+        Texture2D player_texture;
+        Texture2D bullet_texture;
+        List<Texture2D> enemy_textures;
+        List<SpriteBatch> enemyspriteBatchs = new List<SpriteBatch>();
+        List<SpriteBatch> bulletspriteBatchs = new List<SpriteBatch>();
+        Random cRandom = new System.Random();
+
+        Player player;
+        public int bulletexist = 0;
+        public int enemyexist = 0;
+        List<Bullet> bullets = new List<Bullet>();
+        List<Enemy> enemys = new List<Enemy>();
+        public int frame=0;
+
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+        }
+
+        /// <summary>
+        /// Allows the game to perform any initialization it needs to before starting to run.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content.  Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+            base.Initialize();
+        }
+
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+            player_texture = Content.Load<Texture2D>("hex1.png");
+            player = new Player(0, 0, 6, player_texture, spriteBatch);
+
+            bullet_texture = Content.Load<Texture2D>("hex1.png");
+            //bullet = new Bullet(player.x,player.y,0,5,1,bullet_texture,spriteBatch);
+            //bullets.Add(new Bullet(player.x, player.y, 0, 5, 1, bullet_texture, spriteBatch));
+
+            enemy_textures=new List<Texture2D>();
+            enemy_textures.Add(Content.Load<Texture2D>("hex1.png")) ;
+            // TODO: use this.Content to load your game content here
+        }
+
+        /// <summary>
+        /// UnloadContent will be called once per game and is the place to unload
+        /// game-specific content.
+        /// </summary>
+        protected override void UnloadContent()
+        {
+            player_texture.Dispose();
+            enemy_textures[0].Dispose();
+            bullet_texture.Dispose();
+            // TODO: Unload any non ContentManager content here
+        }
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        /// 
+
+
+
+        protected override void Update(GameTime gameTime)//mainloop
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            player.move();
+            player.shot(bullet_texture);
+           
+
+           
+            int iRandom = cRandom.Next(600);
+            iRandom = cRandom.Next(600);
+            if (frame % 100 == 0)
+            {
+                enemyspriteBatchs.Add(new SpriteBatch(GraphicsDevice));
+                enemys.Add(new Enemy(iRandom, 0, 0, 1, 1, enemy_textures[0], enemyspriteBatchs[frame/100]));
+                enemyexist++;
+            }
+            
+            if (enemyexist>0)
+            {
+                for (int i = 0; i < enemys.Count; i++)
+                {
+                    enemys[i].move();
+                }
+            }
+            /*
+            if (bulletexist > 0)
+            {
+                for (int i = 0; i < bullets.Count; i++)
+                {
+                    bullets[i].move();
+                }
+            }
+            else { player.x = 100;player.y = 100; }
+            */
+
+            frame++;
+            // TODO: Add your update logic here
+
+            base.Update(gameTime);
+        }
+
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        /// 
+
+
+        protected override void Draw(GameTime gameTime)//render
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+            spriteBatch.Begin();
+            for (int i = 0;i < enemyspriteBatchs.Count;i++)
+            {
+                enemyspriteBatchs[i].Begin();
+            }
+            for (int i = 0; i < bulletspriteBatchs.Count; i++)
+            {
+                bulletspriteBatchs[i].Begin();
+            }
+
+            //if (bulletexist>0) {for(int i = 0; i < bullets.Count; i++){ bullets[i].draw();} }
+            if(enemyexist>0){ for (int i = 0; i < enemys.Count; i++) { enemys[i].draw(); } }
+            player.draw();
+
+            spriteBatch.End();
+            for (int i = 0; i < enemyspriteBatchs.Count; i++)
+            {
+                enemyspriteBatchs[i].End();
+            }
+            for (int i = 0; i < bulletspriteBatchs.Count; i++)
+            {
+                bulletspriteBatchs[i].End();
+            }
+            // TODO: Add your drawing code here
+
+            base.Draw(gameTime);
+        }
+    }
+}
+
---
+++ toruyohpractice/Game1/Inputmanager.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+
+namespace cellgame
+{
+    /// <summary>
+    /// マウスやキーボードからの入力を行うクラス
+    /// </summary>
+    abstract class InputManager
+    {
+        public bool IsEnd { get; protected set; }
+        public abstract bool IsKeyDown(KeyID k);
+        public abstract bool IsKeyDownOld(KeyID k);
+        static readonly int keys = Function.GetEnumLength(typeof(KeyID));
+        int[] times = new int[keys];
+        public int KeyDownTime(KeyID k)
+        {
+            return times[(int)k];
+        }
+
+        public bool IsPressedForMenu(KeyID id, int wait, int interval)
+        {
+            return GetKeyPressed(id) || (KeyDownTime(id) >= wait && KeyDownTime(id) % interval == 0);
+        }
+
+        public bool IsAnyKeyDown()
+        {
+            foreach (KeyID id in Enum.GetValues(typeof(KeyID)))
+                if (IsKeyDown(id))
+                    return true;
+            return false;
+        }
+
+        protected static Keys[] KeyIDKeys;
+
+        static InputManager()
+        {
+            KeyIDKeys = new Keys[Enum.GetNames(typeof(KeyID)).Length];
+            SetDefault();
+        }
+        public static void SetDefault()
+        {
+            SetKey(KeyID.Up, Keys.Up);
+            SetKey(KeyID.Down, Keys.Down);
+            SetKey(KeyID.Left, Keys.Left);
+            SetKey(KeyID.Right, Keys.Right);
+            SetKey(KeyID.Select, Keys.Z);
+            SetKey(KeyID.Cancel, Keys.X);
+            SetKey(KeyID.Wait, Keys.C);
+            SetKey(KeyID.Skip, Keys.LeftControl);
+            SetKey(KeyID.N, Keys.N);
+            SetKey(KeyID.Slow, Keys.LeftShift);
+        }
+        public static void SetKey(KeyID id, Keys k)
+        {
+            KeyIDKeys[(int)id] = k;
+        }
+        public static Keys GetKey(KeyID id) { return KeyIDKeys[(int)id]; }
+        public static Keys[] GetKeys() { return KeyIDKeys; }
+        public static void SetKeys(Keys[] keys) { KeyIDKeys = keys; }
+
+        public void Update()
+        {
+            Update_();
+            for (int i = 0; i < keys; i++)
+                if (IsKeyDown((KeyID)i)) times[i]++;
+                else times[i] = 0;
+        }
+        public abstract void Update_();
+        public virtual void End() { }
+        /// <summary>
+        /// キーの状態を調べる
+        /// </summary>
+        /// <param name="key">調べるキー</param>
+        /// <returns>そのフレームに押されたかなどの情報</returns>
+        public KeyState GetKeyState(KeyID key)
+        {
+            if (IsKeyDown(key))
+                if (IsKeyDownOld(key))
+                    return KeyState.Down;
+                else
+                    return KeyState.Press;
+            else
+                if (IsKeyDownOld(key))
+                return KeyState.Pull;
+            else
+                return KeyState.Up;
+        }
+        /// <summary>
+        /// GeKeyState == KeyState.Pressと同義
+        /// </summary>
+        public bool GetKeyPressed(KeyID key) { return times[(int)key] == 1; }
+    }
+    class KeyManager : InputManager
+    {
+        KeyboardState now;
+        KeyboardState old;
+        int index;
+        public override void Update_()
+        {
+            old = now;
+            now = Keyboard.GetState();
+            index++;
+        }
+        public override bool IsKeyDown(KeyID k) { return now.IsKeyDown(GetKey(k)); }
+        public override bool IsKeyDownOld(KeyID k) { return old.IsKeyDown(GetKey(k)); }
+    }
+
+    /// <summary>
+    /// キーの状態（4種）
+    /// </summary>
+    enum KeyState
+    {
+        /// <summary>キーに触られていない状態</summary>
+        Up,
+        /// <summary>キーが離された瞬間</summary>
+        Pull,
+        /// <summary>キーを押した瞬間</summary>
+        Press,
+        /// <summary>キーが押されている状態</summary>
+        Down
+    }
+    enum KeyID
+    {
+        Up, Down, Left, Right,
+        Select, Cancel, Wait, Skip,Slow,
+        //以下デバッグ用
+        N,
+    }
+    /// <summary>
+    /// マウスのボタン（未実装）
+    /// </summary>
+    enum MouseButton
+    {
+        Left, Middle, Right
+    }
+}
